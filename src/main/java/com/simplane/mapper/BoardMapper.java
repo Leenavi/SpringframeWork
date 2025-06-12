7e99389a8770b1f5219fde3569b22865f43205f8
Leenavi <tlsrl930@gmail.com>
2025-06-12 19:12:10 +0900

    첫 커밋

--- src/main/java/com/simplane/mapper/BoardMapper.java
+++ src/main/java/com/simplane/mapper/BoardMapper.java
@@ -2,9 +2,13 @@ package com.simplane.mapper;
 
 import com.simplane.domain.BoardVO;
 
+import java.util.List;
+
 public interface BoardMapper {
 
     public BoardVO read(Long boardid);
+
+    public List<BoardVO> readAll();
 
     public void create(BoardVO board);
 
