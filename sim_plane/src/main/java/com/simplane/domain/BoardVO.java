6ebcc450fe168da4ea50050f18fba291785747fb
Lee <tlsrl930@gmail.com>
2025-06-15 22:54:57 +0900

    첫 커밋

---
+++ sim_plane/src/main/java/com/simplane/domain/BoardVO.java
@@ -0,0 +1,12 @@
+package com.simplane.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+
+    private Long boardid;
+    private String title;
+    private String content;
+    private String imagePath;
+    private Date regDate;
+}
